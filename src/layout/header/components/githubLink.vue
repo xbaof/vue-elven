40f6523995578c0fcda8d70ac8965467b97abc4e
gujianchang <gujianchang@123.com>
2026-03-09 17:30:13 +0800

    feat(ui): add github entry in header and dashboard

---
+++ src/layout/header/components/githubLink.vue
@@ -0,0 +1,15 @@
+<template>
+  <a :href="repositoryUrl" target="_blank" rel="noopener noreferrer" title="GitHub">
+    <svg-icon :size="18" :icon="githubIcon" />
+  </a>
+</template>
+
+<script setup lang="ts">
+import githubIcon from '@iconify-icons/icon-park-outline/github'
+
+defineOptions({
+  name: 'GithubLink'
+})
+
+const repositoryUrl = 'https://github.com/xbaof/vue-elven'
+</script>
